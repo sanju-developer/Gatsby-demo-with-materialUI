9c3dab46f2885577d841372b518a7a053ad538dd
appinventiv <vivek.rajoriya@appinventiv.com>
2020-11-03 10:35:55 +0530

    fix my wardrobe routing

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -4,12 +4,14 @@ import Home from "./home/index.js"
 import DetailPage from "./home/detail"
 import Login from "./login"
 import PrivateRoute from "../components/privateRoute"
+import MyWardrobe from "./my-wardrobe/index.js"
 
 const Routing = (props) => {
   return (
     <Router>
       <PrivateRoute path="/app/home" component={Home} />
       <PrivateRoute path="/app/home/detail/:id" component={DetailPage} />
+      <PrivateRoute path="/app/my-wardrobe" component={MyWardrobe}/>
       <Login path="/app/login" />
       <Login path="/" />
     </Router>
