eec16ab0dde0ee8872f086476a2112e5793f0f81
appinventiv <vivek.rajoriya@appinventiv.com>
2020-11-05 10:05:22 +0530

    added additonal check for private roruting

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { getTokenFromLS } from "../services/auth"
 import Routing from "./routes"
 
 const App = props => {
+  useEffect(() => {
+    if (getTokenFromLS()) {
+      navigate("/app/home")
+    }
+  }, [])
+
   return (
     <>
       <Routing {...props} />
