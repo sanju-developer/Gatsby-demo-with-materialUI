ce677b5b143cb839358280e81f4f7386ce041686
appinventiv <vivek.rajoriya@appinventiv.com>
2020-11-05 10:12:24 +0530

    import navigate from innext

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,8 +1,11 @@
+import { useI18next } from "gatsby-plugin-react-i18next"
 import React, { useEffect } from "react"
 import { getTokenFromLS } from "../services/auth"
 import Routing from "./routes"
 
 const App = props => {
+  const { navigate } = useI18next()
+  
   useEffect(() => {
     if (getTokenFromLS()) {
       navigate("/app/home")
